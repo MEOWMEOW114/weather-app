80b3644aafb0b150dc01f416b6c5ca5b43eb925f
Tom Lee <leehc114@gmail.com>
2019-08-20 03:58:22 +0800

    finish reuqminet II

--- src/App.js
+++ src/App.js
@@ -7,53 +7,78 @@ import DailyForecast from './components/Forecast/Daily';
 import CityNotFound from './components/CityNotFound';
 import Loading from './components/Loading';
 import CityInfo from './components/CityInfo';
-// import Footer from './components/Footer';
+import { geolocated } from 'react-geolocated';
 
 import { fetchWeatherForecast, fetchUnitsIfNeeded } from './redux/weatherForcast/actions';
 
 // import './App.scss';
+class App extends React.Component {
+  componentDidUpdate(prevProps) {
+    const { weatherForecast, coords, fetchWeatherForecast } = this.props;
+    const { city, dailyForecast, error, loading, units } = weatherForecast;
 
-const App = ({ fetchWeatherForecast, weatherForecast, fetchUnitsIfNeeded }) => {
-  const { city, dailyForecast, error, loading, units } = weatherForecast;
-  console.log(error);
-  return (
-    <div className="App">
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="/">Weather Forecast</Navbar.Brand>
-        <SearchBar
-          units={units}
-          onUnitsChange={fetchUnitsIfNeeded}
-          onSubmit={fetchWeatherForecast}
-        />
-      </Navbar>
-      <Container>
-        <Row className="justify-content-md-center">
-          <Col xs lg="8">
-            {/* {!error && !dailyForecast && !loading && <Welcome />} */}
-            {error && !loading && <CityNotFound message={error.message} />}
-          </Col>
-        </Row>
-        {!loading && city && (
-          <Row className="city-info">
-            <Col xs lg="auto">
-              {!loading && dailyForecast && <CityInfo city={city} />}
-            </Col>
-          </Row>
-        )}
-        {!loading && dailyForecast && (
+    // Feature requirment II
+    // The first time the user comes to the site, we want the query to be by their current
+    // location.
+    if (!city && !dailyForecast && coords !== prevProps.coords) {
+      console.log(coords);
+      fetchWeatherForecast({
+        latlng: coords.latitude + ',' + coords.longitude,
+        units,
+      });
+    }
+  }
+  render() {
+    const {
+      fetchWeatherForecast,
+      weatherForecast,
+      fetchUnitsIfNeeded,
+      isGeolocationAvailable,
+      isGeolocationEnabled,
+      coords,
+    } = this.props;
+    const { city, dailyForecast, error, loading, units } = weatherForecast;
+    console.log(weatherForecast);
+
+    return (
+      <div className="App">
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="/">Weather Forecast</Navbar.Brand>
+          <SearchBar
+            units={units}
+            onUnitsChange={fetchUnitsIfNeeded}
+            onSubmit={fetchWeatherForecast}
+          />
+        </Navbar>
+        <Container>
           <Row className="justify-content-md-center">
-            <Col xs>
-              <DailyForecast dailyForecast={dailyForecast} units={units} />
+            <Col xs lg="8">
+              {/* {!error && !dailyForecast && !loading && <Welcome />} */}
+              {error && !loading && <CityNotFound message={error.message} />}
             </Col>
           </Row>
-        )}
-        <Row className="justify-content-md-center">
-          <Col xs>{loading && <Loading />}</Col>
-        </Row>
-      </Container>
-    </div>
-  );
-};
+          {!loading && city && (
+            <Row className="city-info">
+              <Col xs lg="auto">
+                {!loading && dailyForecast && <CityInfo city={city} />}
+              </Col>
+            </Row>
+          )}
+          {!loading && dailyForecast && (
+            <Row className="justify-content-md-center">
+              <Col xs>
+                <DailyForecast dailyForecast={dailyForecast} units={units} />
+              </Col>
+            </Row>
+          )}
+          <Row className="justify-content-md-center">
+            <Col xs>{loading && <Loading />}</Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = state => {
   return {
@@ -61,7 +86,14 @@ const mapStateToProps = state => {
   };
 };
 const mapDispatchToProps = { fetchWeatherForecast, fetchUnitsIfNeeded };
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(App);
+export default geolocated({
+  positionOptions: {
+    enableHighAccuracy: false,
+  },
+  userDecisionTimeout: 5000,
+})(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps,
+  )(App),
+);
